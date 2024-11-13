a4d716ca14b37e43381cd2ad3782e9a842b9b1bb
shlomiash <shlomiasssh1999@gmail.com>
2024-11-13 15:34:04 +0200

    Adding notes and removing some unecessary code

--- components/admin/dashboard/filter-promotions-button.tsx.tsx
+++ components/admin/dashboard/filter-promotions-button.tsx.tsx
@@ -4,7 +4,7 @@ import ExportCVButton from "./reports/export-cv-button";
 
 export default function FilterPromotionsButtons(){
     return (
-        <div className="w-full flex justify-end gap-6 mt-8 mb-8 px-4">
+        <div className="w-full flex justify-end gap-6 mt-8 mb-8">
             <ExportCVButton/>
             <CouponsByDate/>
             <CouponsByUser/>
