f138915b8af4fbe18bf9ebfe92cea33bad5cee42
shlomiash <shlomiasssh1999@gmail.com>
2024-11-13 14:29:47 +0200

    exporting button works!

--- components/admin/dashboard/reports/export-cv-button.tsx
+++ components/admin/dashboard/reports/export-cv-button.tsx
@@ -1,11 +1,25 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
+import { getDiscounts } from "@/server/get-data";
+import * as XLSX from "xlsx";
 
 
 export default function ExportCVButton(){
+
+
+    const exportDataToExcel = async (title?: string, worksheetname?: string) => {
+       const data = await getDiscounts();
+       // Create Excel workbook and worksheet
+       const workbook = XLSX.utils.book_new();
+       const worksheet = XLSX.utils?.json_to_sheet(data);
+       XLSX.utils.book_append_sheet(workbook, worksheet, worksheetname);
+       // Save the workbook as an Excel file
+       XLSX.writeFile(workbook, `${title}.xlsx`);
+      };
+
     return (
-        <Button>
+        <Button onClick={ ()=> exportDataToExcel('discount' , 'ExportDiscount')}>
             Export CV
         </Button>
     )
