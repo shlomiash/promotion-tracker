625738d9c3249cafdd559f690c64deccdc2a083c
shlomiash <shlomiasssh1999@gmail.com>
2025-01-08 20:16:36 +0200

    changes for vercel

--- components/admin/dashboard/table/delete-button.tsx
+++ components/admin/dashboard/table/delete-button.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { Discount } from "@/server/data/discounts";
 import { handleDeleteDiscount } from "@/server/actions/handle-delete-button";
-import { useRouter } from "next/navigation";
 
 export function DeleteButton({ discount }: { discount: Discount }) {
   return (
